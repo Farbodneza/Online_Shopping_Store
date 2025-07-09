78d2f9e9839f08aff659d7f556bc4b90591dce0f
Farbod Nezafati <farbodneza@gmail.com>
2025-07-09 11:54:49 +0330

    add(account.serializers): add Customer register and login serializers.

---
+++ account/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from account.models import Customer
+
+
+class CustomerRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['username', 'email', 'password']
+    def create(self, validated_data):
+        user = Customer(
+            username=validated_data['username'],
+            email=validated_data['email']
+            )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+    def validate(self, attrs):
+        if attrs["username"] == attrs["password"]:
+            raise serializers.ValidationError('username and password should be deferent')
+        return attrs
+    
+    
+class CustomuserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)

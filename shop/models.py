e4434a28d3662907e641253d2a3ee75562203f44
Farbod Nezafati <farbodneza@gmail.com>
2025-07-12 18:13:13 +0330

    add(shop.models): add category model.

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,13 @@
 from django.db import models
-from account.models import Customer
+from account.models import Customer, Address
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    image = models.ImageField()
+    is_active = models.BooleanField()
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL,  null=True, blank=True)
+
+
+

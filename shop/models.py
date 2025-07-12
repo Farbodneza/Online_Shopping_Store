cc77545463cb4161f2523146b78995a5031de144
Farbod Nezafati <farbodneza@gmail.com>
2025-07-12 18:16:17 +0330

    add(shop.models): add product model.

--- shop/models.py
+++ shop/models.py
@@ -10,4 +10,18 @@ class Category(models.Model):
     parent = models.ForeignKey('self', on_delete=models.SET_NULL,  null=True, blank=True)
 
 
+class Product(models.Model):
+    name = models.TextField(max_length=150, unique=True)
+    image = models.ImageField(upload_to='product_pictures')
+    description = models.TextField()
+    best_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    rating = models.DecimalField()
+    categories = models.ManyToManyField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField()
+    def __str__(self):
+        return self.name
+    
+
 

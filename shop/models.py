962cc4c940d9dd4464f9ee687677cc0af06fbd27
Farbod Nezafati <farbodneza@gmail.com>
2025-07-12 18:42:05 +0330

    add(shop.models): add StoreItem model.

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     name = models.CharField()
     description = models.TextField()
     image = models.ImageField()
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     parent = models.ForeignKey('self', on_delete=models.SET_NULL,  null=True, blank=True)
 
 
@@ -19,7 +19,7 @@ class Product(models.Model):
     rating = models.DecimalField()
     categories = models.ManyToManyField()
     created_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     def __str__(self):
         return self.name
     
@@ -33,5 +33,13 @@ class Store(models.Model):
 
 class Seller(models.Model):
     user = models.OneToOneField(Customer, on_delete=models.CASCADE)
+
+
+class StoreItem(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
 
 

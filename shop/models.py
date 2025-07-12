ee60718c8ab29b5237ce8d98fff85f2d058355cd
Farbod Nezafati <farbodneza@gmail.com>
2025-07-12 18:52:36 +0330

    add(shop.models): add Order and OrderItem models.

--- shop/models.py
+++ shop/models.py
@@ -41,5 +41,29 @@ class StoreItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
+    
+
+class Order(models.Model):
+    STATUS_CHOCES = [
+        (1, 'Pending'),
+        (2, 'Processing'),
+        (3, 'Delivered'),
+        (4, 'Cancelled'),
+        (5, 'FAILED'),
+        
+    ]
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders', default=None)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.IntegerField(choices=STATUS_CHOCES, default=1)
+    shipping_address = models.ForeignKey(Address, on_delete=models.PROTECT, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    store_item = models.ForeignKey(StoreItem, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price  = models.DecimalField(max_digits=10, decimal_places=2)
 
 

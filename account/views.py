f51c95fc4634c1f7f93d3f41b849d9f0d50ef92e
Farbod Nezafati <farbodneza@gmail.com>
2025-07-17 11:03:58 +0330

    add(account): add Register APIView.

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login, logout
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from account.models import CustomUser, Address
+from account.serializers import CustomuserRegisterSerializer, CustomuserLoginSerializer
 
-# Create your views here.
+class RegisterUserAPIView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomuserRegisterSerializer
+
+

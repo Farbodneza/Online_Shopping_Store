72e12de044f07ef8d023aad647f072bca782169b
Farbod Nezafati <farbodneza@gmail.com>
2025-07-17 11:05:07 +0330

    add(account.views): add Login ApiView, handle with username and pass.

--- account/views.py
+++ account/views.py
@@ -14,3 +14,28 @@ class RegisterUserAPIView(generics.CreateAPIView):
     serializer_class = CustomuserRegisterSerializer
 
 
+class LoginUserAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = CustomuserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user:
+            refresh = RefreshToken.for_user(user)
+            user_serializer = CustomuserRegisterSerializer(user)
+            login(request, user)  
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': user_serializer.data
+            }, status=status.HTTP_200_OK)
+
+        return Response({'error': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+
+
+    
+

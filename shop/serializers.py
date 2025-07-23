915d28757082c427b8314a9f1f97d31cc594f356
Farbod Nezafati <farbodneza@gmail.com>
2025-07-23 15:52:26 +0330

    add(shop.serializers): add ProductImageSerializer, ProductSerializer and ShopSerializer.

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from shop.models import Product
+from shop.models import Product, shop, ProductImage
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields= "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(source='productimage_set', many=True)
     class Meta:
         model = Product
+        fields = "__all__"
 
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = shop 
+        fields= "__all__"

f175a69ca0edad4e61b3c281cb40715857097faf
Farbod Nezafati <farbodneza@gmail.com>
2025-07-23 15:52:59 +0330

    add(shop.views): add ManageProductAPIViewSet and ManageStoreAPIViewSet.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login, logout
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from shop.models import Product, Store, StoreItem, ProductImage
+from django.core.cache import cache
+from rest_framework.decorators import action
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated , AllowAny, IsAdminUser
+from shop.permissions import IsSeller, IsShopOwner
+from shop.serializers import (ProductSerializer,)
 
-# Create your views here.
+
+class ManageProductAPIViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsSeller]
+
+
+class ManageStoreAPIViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = ProductSerializer
+    
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated, IsSeller, IsShopOwner]
+        elif self.action == 'create':
+            permission_classes = [IsAuthenticated, IsSeller]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+

f76cdb53f8eb0fc4acb73a9b4be55b350da6f751
Farbod Nezafati <farbodneza@gmail.com>
2025-07-27 12:08:56 +0330

    add(account.urls): add MyProfileAPIView to urls.

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path, include
-from account.views import RegisterUserAPIView, LoginUserAPIView, LogoutUserAPIView, RequestOTPAPIView,VerifyOTPAPIView, ProfileManagmentAPIView, AddressManagerAPIViewSet
+from account.views import RegisterUserAPIView, LoginUserAPIView, LogoutUserAPIView, RequestOTPAPIView,VerifyOTPAPIView, MyProfileAPIView, AddressManagerAPIViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'profiles', ProfileManagmentAPIView, basename='profile')
 router.register(r'addresses', AddressManagerAPIViewSet, basename='address')
 
 
 urlspatterns = [
     path('auth/register/', RegisterUserAPIView.as_view()),
+    path('profile/me/', MyProfileAPIView.as_view(), name='my-profile'),
     path('auth/login/', LoginUserAPIView.as_view()),
     path('auth/logout/', LogoutUserAPIView.as_view()),
-    path('auth/otp/', RequestOTPAPIView.as_view()),
+    path('auth/get_otp/', RequestOTPAPIView.as_view()),
     path('auth/confirm_otp/', VerifyOTPAPIView.as_view()),
     path('', include(router.urls)),
 ]
